2693681b7059b801bb6774957a35050017a7b15c
Yusuf <yusuffaltuun@gmail.com>
2026-07-15 18:30:08 +0300

    feat: integrate causal masking to self-attention module to prevent look-ahead

--- src/models/attention.py
+++ src/models/attention.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 
 
 class SelfAttention (nn.Module):
-    def __init__(self, emb_dim: int, head_dim: int, dropout_rate: float = 0.1):
+    def __init__(self, emb_dim: int, head_dim: int, dropout_rate: float = 0.1 , max_len: int = 1024):
         super().__init__()
         self.head_dim = head_dim
         self.W_query = nn.Linear(emb_dim, head_dim, bias=False)
@@ -15,12 +15,22 @@ class SelfAttention (nn.Module):
         
         
         self.dropout = nn.Dropout(dropout_rate)
+
+
+        self.register_buffer(
+            "mask", 
+            torch.tril(torch.ones(max_len, max_len)).view(1, 1, max_len, max_len)
+        )
     
     def forward(self,x):
         keys = self.W_key(x)
         values = self.W_value(x)
         queries = self.W_query(x)
         attn_scores = queries @ keys.transpose(1, 2)
+
+        T = x.size(1)
+
+        attn_scores = attn_scores.masked_fill(self.mask[0, 0, :T, :T] == 0, -1e9)
         attn_weights = F.softmax(
             attn_scores / math.sqrt(self.head_dim), dim=-1)
         attn_weights = self.dropout(attn_weights)
